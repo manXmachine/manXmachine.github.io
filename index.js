633ff5a20f7011a78efd81338fe79313edc4bccd
waynemcfetridge <wayne@thevariousartists.com.au>
2020-06-09 21:08:24 +1000

    working

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const vm = new window.Vue({
 			b : 1,
 			osc : 0,
 			t :0,
-			NUM_LINES:10,
+			NUM_LINES:20,
 			colorsPalette : ["#FF4500","#FC9900","#99AA99"]
 			
 		}
@@ -39,8 +39,19 @@ const vm = new window.Vue({
     },
     
     draw(sketch){
-    	this.lines(1,242,226,196,this.t*2,1200);
-    	this.t += 0.01;
+    	sketch.background(0);0
+    	this.lines(sketch,1,242,226,196,this.t*2,sketch.windowHeight/2);
+    	this.lines(sketch,1,242,185,15,this.t*5,1280);
+    this.lines(sketch,1,242,121,15,this.t,1240);
+     this.lines(sketch,1,217,43,4,this.t,540);
+     this.lines(sketch,1,28,108,140,this.t,740);
+     this.lines(sketch,1,242,226,196,this.t*2,100);
+    this.lines(sketch,1,242,185,15,this.t*.5,380);
+    this.lines(sketch,1,242,121,15,this.t,250);
+     this.lines(sketch,1,217,43,4,this.t,140);
+     this.lines(sketch,1,28,108,140,this.t,640);
+    	this.t += 1;
+    	console.log("this.t",this.t);
 	},
 
  	postMood : function () {
@@ -60,36 +71,36 @@ const vm = new window.Vue({
 		},
 	lines : function(sketch,w, r,  g,  b, _t, f) {
 
-		    sketch.strokeWeight(w);
-		    sketch.pushMatrix();
-		    sketch.translate(width/2, height/2);
+		   // sketch.strokeWeight(w);
+		    sketch.push();
+		    sketch.translate(sketch.windowWidth/2,sketch.windowHeight/2);
 		    for (var i = 0; i < this.NUM_LINES; i++) {
 		       sketch.stroke(r,g,b, sketch.map(i,0,this.NUM_LINES,120,255));
 		       sketch.fill(r,g,b, sketch.map(i,0,this.NUM_LINES,120,255));
-		       index = sketch.random(this.words.length);
+		       //index = sketch.random(this.words.length);
 		      
-		       sketch.line(x1(sketch,_t + i,f), y1(sketch,_t + i,f), x2(sketch,_t + i,f), y2(sketch,_t + i,f)); 
-		       sketch.pushMatrix();
-		        sketch.translate(x1(_t + i,f)+10, y1(_t + i,f));
-		        sketch.rotate(radians(90));
-		        sketch.text(words[index],0,0 );
-		       sketch.popMatrix();
+		       sketch.line(this.x1(sketch,_t + i,f), this.y1(sketch,_t + i,f), this.x2(sketch,_t + i,f), this.y2(sketch,_t + i,f)); 
+		       //sketch.push();
+		       // sketch.translate(x1(_t + i,f)+10, y1(_t + i,f));
+		        //sketch.rotate(radians(90));
+		        //sketch.text(words[index],0,0 );
+		       //sketch.pop();
     			}
-    		sketch.popMatrix();
+    		sketch.pop();
 		},
-	 x1(sketch,t,  f){
+	 x1 : function(sketch,t,  f){
 	     return sketch.sin(t/10) * f + sketch.sin(t/5) * f; 
 		},
 
-	 y1(sketch, t, f){
+	 y1 : function(sketch, t, f){
 	     return sketch.cos(t/10) * f; 
 		},
 
-	 x2(sketch, t, f){
+	 x2 : function(sketch, t, f){
 	     return sketch.sin(t/10) * f + sketch.sin(t) * 2; 
 		},
 
-	 y2( sketch,t, f){
+	 y2 : function( sketch,t, f){
 	     return sketch.cos(t/20) * f + sketch.cos(t/12) * f;
 		},
 	},
