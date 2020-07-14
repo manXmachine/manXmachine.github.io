628171d29f78b902a5075182a1f3d202a85a1dff
Skye Johnson <skye@thevariousartists.com.au>
2020-07-14 17:05:43 +1000

    big data stricture changes to suppriot more data

--- app.js
+++ app.js
@@ -37,10 +37,9 @@ const vm = new window.Vue({
       twoWeight:2,
       twoDiv:1,
 
-			record:false,
-      recordingStarted:false,
+			others : [{test:"test",weight:10, count:100,speed:5,pulse:10}],
 			
-			colorsPalette : ["#FF4500","#FC9900","#99AA99"],
+			colorsPalette : ["#a8e6fb","#faea68","#427abb","#e74865","#6f2e52","#d94d59","#25345b","#aa4853"],
 			
 		}
 	},
@@ -49,135 +48,65 @@ const vm = new window.Vue({
 
 	 setup(sketch) {
 		  sketch.createCanvas(sketch.windowWidth,sketch.windowHeight);
-      this.pg = sketch.createGraphics(300, 300);
       sketch.strokeCap(sketch.SQUARE);
     },
     
     draw(sketch){
       
     	sketch.background(0);
-
-      if(this.record != true){
-
-
-    	
-      if(this.oneCount != 0){
-        this.lines(sketch,this.oneWeight/10,217,43,4,this.t*this.oneSpeed/100,sketch.windowHeight/2.5,this.oneDiv,this.oneCount/10);
-        this.lines(sketch,this.oneWeight/10,28,108,140,this.t*this.oneSpeed/100,sketch.windowHeight/2.5+sketch.sin(this.t/10)*this.onePulse/100,this.oneDiv,this.oneCount/10);
-      }
-
       
-       if(this.twoCount != 0){
-        this.lines(sketch,this.twoWeight/10,217,43,4,this.t*this.twoSpeed/100,sketch.windowHeight/2.5,this.twoDiv,this.twoCount/10);
-        this.lines(sketch,this.twoWeight/228,108,140,this.t*this.twoSpeed/100,sketch.windowHeight/2.5+sketch.sin(this.t/10)*this.twoPulse/10,this.twoDiv,this.twoCount/10);
-        }
-	 
-
-
-      this.lines(sketch,this.myWeight/10,242,226,196,this.t*this.mySpeed/100,sketch.windowHeight/4-sketch.sin(this.t)*this.myPulse/10,this.myDiv,this.myCount/10);
-      this.lines(sketch,this.myWeight/10,242,185,15,this.t*this.mySpeed/100,sketch.windowHeight/4+sketch.sin(this.t/10)*this.myPulse/10,this.myDiv,this.myCount/10);
-
-	    }
-    	
-    	
-    if(this.record == true){
-
-      if(this.recordingStarted == false){
-    	  this.recordingStarted = true;
-        this.capturer.start();
-       }
+    
+      for (var i = 0; i < this.others.length; i++) {
+        //console.log(this.colorsPalette[i]);
+        this.lines(sketch,this.others[i].weight/10,this.colorsPalette[i],this.t*this.others[i].speed/200,sketch.windowHeight/(2.5),Number(this.others[i].count)/10,this.others[i].pulse/2);
+        //this.lines(sketch,1,this.colorsPalette[i],this.t*this.others[i].speed/100,sketch.windowHeight/(2.5+i/4)+sketch.sin(this.t/10)*this.others[i].pulse/100,this.others[i].div,Number(this.others[i].count)/10);
+      }
         
-    	this.pg.background(0);
-      this.pg.strokeCap(sketch.SQUARE);
-    	
+      //this.lines(sketch,this.myWeight/10,this.colorsPalette[0],this.t*this.mySpeed/100,sketch.windowHeight/4,this.myCount/10,this.myPulse);
+      //this.lines(sketch,this.others[0].weight/10,this.colorsPalette[0],this.t*this.others[0].speed/200,sketch.windowHeight/(10),Number(this.others[0].count)/10,this.others[0].pulse/2);
+        
+      //this.lines(sketch,1,this.colorsPalette[1],this.t*this.mySpeed/100,sketch.windowHeight/4+sketch.sin(this.t/10)*this.myPulse/10,this.myDiv,this.myCount/10);
 
-       if(this.oneCount != 0){
-        this.lines(this.pg,this.oneWeight/10,217,43,4,this.t*this.oneSpeed/100,sketch.windowHeight/5,this.oneDiv,this.oneCount/10);
-        this.lines(this.pg,this.oneWeight/10,28,108,140,this.t*this.oneSpeed/100,sketch.windowHeight/5+sketch.sin(this.t/10)*this.onePulse/100,this.oneDiv,this.oneCount/10);
-      }
-
-      
-       if(this.twoCount != 0){
-        this.lines(this.pg,this.twoWeight/10,217,43,4,this.t*this.twoSpeed/100,sketch.windowHeight/5,this.twoDiv,this.twoCount/10);
-        this.lines(this.pg,this.twoWeight/228,108,140,this.t*this.twoSpeed/100,sketch.windowHeight/5+sketch.sin(this.t/10)*this.twoPulse/10,this.twoDiv,this.twoCount/10);
-        }
-      
-
-      this.lines(this.pg,this.myWeight/15,242,226,196,this.t*this.mySpeed/100,sketch.windowHeight/10-sketch.sin(this.t)*this.myPulse/10,this.myDiv,this.myCount/10);
-      this.lines(this.pg,this.myWeight/15,242,185,15,this.t*this.mySpeed/100,sketch.windowHeight/10+sketch.sin(this.t)*this.myPulse/10,this.myDiv,this.myCount/10);
-
-
-	    //this.lines(this.pg,1,227,43,4,this.t/2,sketch.windowHeight/3.5);
-	    //this.lines(this.pg,1,228,108,140,this.t/2,sketch.windowHeight/3.5+sketch.sin(this.t)*this.pulse);
-	    this.capturer.capture(this.pg.canvas);
-    		
-    	
-    	if(sketch.frameCount % 300 == 0){
-
-        this.record = false;
-        this.recordingStarted = false;
-    	  this.capturer.stop()
-        this.capturer.save()
-
-       }
-
-      }
-
-      this.t += .2;
+  	 this.t += .2;
 	 },
 
   handleShare: function(){
-      this.record = true;
-      
+      var json = btoa(JSON.stringify(this.others));
+      console.log("json",json)
     },
 
 	handleSave: function(){
-    	
-      
-      this.getOthers();
-      
+    	this.getOthers();
       this.postMine();
+      
+      
+     
     },
 
   getOthers() {
       let root = this
       //Starting at the 5th item, return 3 items from a collection of 10
-      axios.get(this.url+"?limit=20&q=splice(18,2)").then((response) => {
-        console.log('others', response.data)
-
-        //root.currentMood = response.data.mood
-        this.oneCount = response.data[0].count;
-        this.onePulse = response.data[0].pulse;
-        this.oneSpeed = response.data[0].speed;
-        this.oneWeight = response.data[0].weight;
-        this.oneDiv = response.data[0].div;
-
-
-        this.twoCount = response.data[1].count;
-        this.twoPulse = response.data[1].pulse;
-        this.twoSpeed = response.data[1].speed;
-        this.twoWeight = response.data[1].weight;
-        this.twoDiv = response.data[1].div;
-
+      axios.get(this.url+"?limit=7").then((response) => {
+      
+      //this.others = [];
+       
+        for (var i = 0; i < response.data.length; i++) {
+            this.others[i+1] = response.data[i] ;
+            
+        }
+      console.log("others:",this.others.length);
       })
     },
 
-   getTwo() {
-      let root = this
-      axios.get(this.url).then((response) => {
-        console.log('Last mood', response.data)
-        root.currentMood = response.data.mood
-      })
-    },
 
  	postMine : function () {
 			let root = this
 			axios.post(this.url, {
-				count:this.myCount,
-        pulse:this.myPulse,
-        speed:this.mySpeed,
-        weight:this.myWeight,
-        div:this.myDiv,
+				count:this.others[0].count,
+        pulse:this.others[0].pulse,
+        speed:this.others[0].speed,
+        weight:this.others[0].weight,
+       
 
        }).then((response) => {
 				console.log('OK:', response)
@@ -185,18 +114,27 @@ const vm = new window.Vue({
 				console.log('ERROR:', response)
 			})
 		},
-	lines : function(sketch,w, r,  g,  b, _t, f, _div, _count) {
+	lines : function(sketch,w, _c, _t, f,  _count, _pulse) {
+
+    //console.log("_count:",_count);
 
 		    sketch.strokeWeight(w);
 		    sketch.push();
 		    sketch.translate(sketch.width/2,sketch.height/2);
-		   
+		    let c = sketch.color(_c);
 		   	
 		    for (var i = 0; i < _count; i++) {
-		       sketch.stroke(r,g,b, sketch.map(i,0,_count,120,255));
-		       sketch.line(this.x1(sketch,_t + i/_div,f), this.y1(sketch,_t + i/_div,f), this.x2(sketch,_t + i/_div,f), this.y2(sketch,_t + i/_div,f)); 
-		   	
-		    }
+           c.setAlpha(sketch.map(i,0,_count,10,255))
+		       sketch.stroke(c);
+		       sketch.line(this.x1(sketch,_t + i,f), this.y1(sketch,_t + i,f), this.x2(sketch,_t + i,f), this.y2(sketch,_t + i,f)); 
+		   	}
+
+        for (var i = 0; i < _count; i++) {
+           c.setAlpha(sketch.map(i,0,_count,50,100))
+           sketch.stroke(c);
+           f = f - sketch.sin(this.t)* _pulse/100;
+           sketch.line(this.x1(sketch,_t + i,f), this.y1(sketch,_t + i,f), this.x2(sketch,_t + i,f), this.y2(sketch,_t + i,f)); 
+        }
 
     		sketch.pop();
 		},
@@ -220,12 +158,20 @@ const vm = new window.Vue({
 
 	created() {
 
-		this.capturer = new CCapture({
-        format: 'gif', workersPath: './',
-        verbose: false, display: false,
-        framerate: 30, frameLimit: 500
+		let uri = window.location.href.split('?');
+    if (uri.length == 2)
+    {
+      let vars = uri[1].split('&');
+      let getVars = {};
+      let tmp = '';
+      vars.forEach(function(v){
+        tmp = v.split('=');
+        if(tmp.length == 2)
+        getVars[tmp[0]] = tmp[1];
       });
-    //this.capturer.start();
+      console.log(getVars);
+      // do 
+    }
   
 	}
 
